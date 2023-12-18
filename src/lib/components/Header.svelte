851959cda30ac8fbf8ade664faa6480fd788a6df
Razvan Bielz <razvan.bielz@gmail.com>
2023-12-18 13:43:57 +0200

    add header & sidebar
    add assets

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Searchbar from './Searchbar.svelte';
+</script>
+
+<header
+	class="sticky left-0 top-0 z-10 flex h-[64px] items-center justify-between bg-white px-4 py-2"
+>
+	<a href="/">
+		<img src="/icons/ytLogo.svg" class="mr-5 md:hidden" alt="Logo" />
+	</a>
+	<div class="hidden items-center gap-4 md:flex">
+		<img
+			src="/icons/burgerIcon.svg"
+			class="cursor-pointer rounded-full p-3 hover:bg-gray-200"
+			alt="Menu"
+		/>
+		<a href="/">
+			<img src="/icons/ytLogo.svg" alt="Logo" />
+		</a>
+	</div>
+
+	<Searchbar />
+
+	<div class="hidden items-center gap-3 md:flex">
+		<img
+			src="/icons/createIcon.svg"
+			class="cursor-pointer rounded-full p-3 hover:bg-gray-200"
+			alt="Create"
+		/>
+		<img
+			src="/icons/notIcon.svg"
+			class="cursor-pointer rounded-full p-3 hover:bg-gray-200"
+			alt="Notifications"
+		/>
+		<img src="/avatar.png" class="h-8 w-8 cursor-pointer rounded-full bg-gray-300" alt="Avatar" />
+	</div>
+</header>
